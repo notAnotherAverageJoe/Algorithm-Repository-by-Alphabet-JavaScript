7d0d9ceee2f6c7880589897e9f6399c7cdff3791
joseph skokan <joeskokan20@gmail.com>
2024-10-07 21:14:36 -0400

    simple rev

---
+++ R/reverseString.js
@@ -0,0 +1,16 @@
+// Create a function that accepts a string and reverses it
+function reverseString(str) {
+  // Split the string into an array of characters
+  let reversedString = str
+    .split("")
+    // Reverse the array of characters
+    .reverse()
+    // Join the reversed array back into a string
+    .join("");
+
+  // Return the reversed string
+  return reversedString;
+}
+
+// Example :Log the reversed string of "hello" to the console
+console.log(reverseString("hello")); // Output: "olleh"
