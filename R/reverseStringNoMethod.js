cd452f1d4b52e1418a19e033eac454e788bfaecf
joseph skokan <joeskokan20@gmail.com>
2024-10-19 07:35:03 -0400

    rev no method

---
+++ R/reverseStringNoMethod.js
@@ -0,0 +1,10 @@
+// reversing a string without the reverse() method
+
+function reverseString(str) {
+  let revStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    revStr += str[i];
+  }
+  return revStr;
+}
+console.log(reverseString("hello"));
