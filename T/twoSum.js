956dee3c852fd975c7bd824ec077d937ba2e248b
joseph skokan <joeskokan20@gmail.com>
2024-10-21 22:16:30 -0400

    twoSum

---
+++ T/twoSum.js
@@ -0,0 +1,25 @@
+// take an array of numbers and a target
+// return the two indexes of the numbers that add up to the target
+// this solution is time complexity O(n) && space complexity of O(n)
+function twoSum(arr, target) {
+  if (!Array.isArray(arr)) {
+    return "Invalid Array";
+  }
+  if (isNaN(target)) {
+    return "Invalid target, enter a valid integer";
+  }
+  let seen = {};
+  for (let i = 0; i < arr.length; i++) {
+    let neededValue = target - arr[i];
+    if (neededValue in seen) {
+      return [seen[neededValue], i];
+    } else {
+      seen[arr[i]] = i;
+    }
+  }
+  return null;
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
+console.log(twoSum([1, 3, 5, 7], 10)); // Output: [2, 3]
+console.log(twoSum([1, 2, 3], 7));
