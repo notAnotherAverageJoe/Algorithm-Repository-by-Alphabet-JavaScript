94b58258a5a83ffbf411f15582b0c66f478134cc
joseph skokan <joeskokan20@gmail.com>
2024-10-21 20:31:20 -0400

    added a small check

--- R/reverseStringNoMethod.js
+++ R/reverseStringNoMethod.js
@@ -1,6 +1,9 @@
 // reversing a string without the reverse() method
 
 function reverseString(str) {
+  if (typeof str !== "string") {
+    return "Invalid input";
+  }
   let revStr = "";
   for (let i = str.length - 1; i >= 0; i--) {
     revStr += str[i];
